3c6a70271b162a659c58391d08a425a544158a3a
MgHtetMyatAung <htetmyataung16000@gmail.com>
2025-10-09 01:39:39 +0630

    feat: add meta tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { Sparkles, Heart, Share2 } from "lucide-react";
 export default function Home() {
   return (
     <div className=" relative w-full min-h-screen  bg-gradient-to-br from-rose-50 via-pink-50 to-rose-100">
-      <div className=" relative z-10 w-full h-screen grid place-items-center">
+      <div className="relative container z-10 w-full h-screen grid place-items-center">
         <div className=" space-y-7 max-w-[700px] 2xl:max-w-[900px] pt-20">
           <h1 className=" text-center text-rose-900 text-balance">
             Wedding Invitation Maker <br />
@@ -89,7 +89,7 @@ export default function Home() {
             width={389}
             height={422}
             alt="heart"
-            className="opacity-[2%] w-[200px] md:w-[500px] h-auto mx-auto mt-[150px] md:mt-20"
+            className="opacity-[2%] w-[200px] md:w-[500px] h-auto mx-auto mt-[150px] lg:mt-20"
           />
         </div>
       </div>
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,7 +4,7 @@ import { Button } from "../ui/button";
 export default function Header() {
   return (
     <div className=" fixed top-0 left-0 w-full bg-rose-50/30 backdrop-blur z-20">
-      <nav className=" container flex justify-between items-center h-[70px]">
+      <nav className=" container flex justify-between items-center h-[80px]">
         <div className="">
           <p className=" text-lg font-semibold text-rose-900 italic">
             Shwe Latt Twal
